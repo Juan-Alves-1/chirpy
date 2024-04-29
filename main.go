65e1e5d3cc93c670139bd7428829b5b4b4d88ce5
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2024-04-29 20:17:36 -0300

    Updated CORS and added a fileserver

--- main.go
+++ main.go
@@ -8,12 +8,24 @@ func main() {
 	// Step 1: Create a new http.ServeMux
 	mux := http.NewServeMux()
 
+	// Create a file server which serves files out of the current directory
+	// The file server uses the '.' (dot) to indicate the current directory
+	fileServer := http.FileServer(http.Dir("."))
+
+	// Use the Handle method to register the file server as the handler for the root path
+	mux.Handle("/", fileServer)
+
 	// Step 2: Wrap that mux in a custom middleware function that adds CORS headers to the response
 	corsMux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			// Respond to the CORS preflight request
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		// Call the original ServeMux with the request
 		mux.ServeHTTP(w, r)
 	})
