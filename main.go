af57af266864405b58cddb253887ecae637c114d
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2024-04-29 21:25:14 -0300

    CORS all headers updated & img uploaded

--- main.go
+++ main.go
@@ -11,16 +11,20 @@ func main() {
 	// Create a file server which serves files out of the current directory
 	// The file server uses the '.' (dot) to indicate the current directory
 	fileServer := http.FileServer(http.Dir("."))
-
 	// Use the Handle method to register the file server as the handler for the root path
 	mux.Handle("/", fileServer)
+
+	// Setup file server for static assets
+	// StripPrefix removes the "/assets" prefix before looking in the assets directory
+	assetsFileServer := http.FileServer(http.Dir("./assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", assetsFileServer))
 
 	// Step 2: Wrap that mux in a custom middleware function that adds CORS headers to the response
 	corsMux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
 			// Respond to the CORS preflight request
 			w.WriteHeader(http.StatusOK)
